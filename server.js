489413a716a6e0a16b9e74f61c13cf7f97847265
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2023-04-29 00:07:35 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-var express = require('express');
-var app = express();  
 
-const jsdom = require('jsdom')
-const dom = new jsdom.JSDOM("")
-const $ = require('jquery')(dom.window) 
+import express from 'express';
+import axios from 'axios'
+var app = express();
+
 const port = process.env.PORT || 3001;
-var imgs = []
-	$(function () {
-		const token = process.env.TOKEN
-		const url = "https://graph.instagram.com/6053160891434854/media?fields=id&access_token=" + token
-		let x = 0 
-		for (x; x < 20; x++) {  
-		$.get(url).then(async function (rep) { 
-		return await $.get('https://graph.instagram.com/' + rep.data[count].id + '?fields=media_type,media_url&access_token=' + token)
-		}).then((rep) => { 
-			imgs.push(rep.media_url)
-			})}
-		})  
-
-setTimeout(()=>{app.get('/fimlList', function(req, res) {
-  console.log('i receive a GET request');
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.json(imgs)
-})},5000)
+let imgs = [];
+function GetUrl() {
+	imgs = [];
+	const token = process.env.TOKEN
+	const url = "https://graph.instagram.com/6053160891434854/media?fields=id&access_token=" + token;
+	let x = 0;
+	let count = -1;
+	for (x; x < 20; x++) {
+		axios.get(url).then(function (rep) {
+			count++
+			return axios.get('https://graph.instagram.com/' + rep.data.data[count].id + '?fields=media_type,media_url&access_token=' + token)
+		}
+		).then((rep) => {
+			imgs.push(rep.data.media_url)
+		})
+	}
+	setTimeout(() => {
+		console.log('urls atualizadas')
+		app.get('/instaURLS', function (req, res) {
+			console.log(imgs)
+			console.log('i receive a GET request');
+			res.setHeader("Access-Control-Allow-Origin", "*")
+			res.json(imgs)
+		})
+	}, 5000)
+}
+setInterval(GetUrl, 60000)
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`); 
+	console.log(`listening on port ${port}`);
 })
