41577ab9c2b63f2a6e1b5420f2d8fd1c9e382e3b
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2023-04-29 00:33:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import axios from 'axios'
 var app = express();
@@ -6,12 +5,13 @@ var app = express();
 const port = process.env.PORT || 3001;
 let imgs = [];
 function GetUrl() {
+	console.log('atualizando URLS')
 	imgs = [];
 	const token = process.env.TOKEN
 	const url = "https://graph.instagram.com/6053160891434854/media?fields=id&access_token=" + token;
 	let x = 0;
 	let count = -1;
-	for (x; x < 4; x++) {
+	for (x; x < 20; x++) {
 		axios.get(url).then(function (rep) {
 			count++
 			return axios.get('https://graph.instagram.com/' + rep.data.data[count].id + '?fields=media_type,media_url&access_token=' + token)
@@ -19,18 +19,14 @@ function GetUrl() {
 		).then((rep) => {
 			imgs.push(rep.data.media_url)
 		})
-	}
-	setTimeout(() => {
-		console.log('urls atualizadas')
-		app.get('/instaURLS', function (req, res) {
-			console.log(imgs)
-			console.log('i receive a GET request');
-			res.setHeader("Access-Control-Allow-Origin", "*")
-			res.json(imgs)
-		})
-	}, 5000)
+	} 
 }
-setInterval(GetUrl, 60000)
+	setInterval(GetUrl, 60000)
+	app.get('/instaURLS', function (req, res) {
+		console.log('i receive a GET request');
+		res.setHeader("Access-Control-Allow-Origin", "*")
+		res.json(imgs)
+	})
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
