83f7cb78a07e642c665c75c30cb6b1b4295b3b60
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2023-04-29 01:04:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ import axios from 'axios'
 var app = express();
 
 const port = process.env.PORT || 3001;
-let imgs = []; 
-let count = 0
-function GetUrl() { 
-	count++ 
-	imgs.push(count)
+let imgs = [];
+function GetUrl() {
+	console.log('atualizando URLS')
+	imgs = [];
+	const token = process.env.TOKEN
+	const url = "https://graph.instagram.com/6053160891434854/media?fields=id&access_token=" + token;
+	let x = 0;
+	let count = -1;
+	for (x; x < 20; x++) {
+		axios.get(url).then(function (rep) {
+			count++
+			return axios.get('https://graph.instagram.com/' + rep.data.data[count].id + '?fields=media_type,media_url&access_token=' + token)
+		}
+		).then((rep) => {
+			imgs.push(rep.data.media_url)
+		})
+	} 
 }
-	setInterval(GetUrl, 15000)
+	setInterval(GetUrl, 60000)
 	app.get('/instaURLS', function (req, res) {
 		console.log('i receive a GET request');
 		res.setHeader("Access-Control-Allow-Origin", "*")
