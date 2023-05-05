fbe792bbf851e8409f690000d78637a151dd3923
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2023-05-05 01:55:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ let imgs = [];
 function GetUrl() {
 	console.log('atualizando URLS')
 	imgs = [];
-	const token = process.env.TOKEN 
+	const token = process.env.TOKEN
 	const token1 = process.env.TOKEN1
-	const url = "https://graph.instagram.com/6053160891434854/media?fields=id&access_token=" + token; 
-	const url1 = "https://graph.instagram.com/6245241412178691/media?fields=id&access_token=" + token1; 
+	const url = "https://graph.instagram.com/6053160891434854/media?fields=id&access_token=" + token;
+	const url1 = "https://graph.instagram.com/6245241412178691/media?fields=id&access_token=" + token1;
 	let x = 0;
-	let count = -1; 
+	let count = -1;
 	axios.get(url1)
 	for (x; x < 20; x++) {
 		axios.get(url).then(function (rep) {
@@ -20,22 +20,37 @@ function GetUrl() {
 			return axios.get('https://graph.instagram.com/' + rep.data.data[count].id + '?fields=media_type,media_url&access_token=' + token)
 		}
 		).then((rep) => {
-			imgs.push({url:rep.data.media_url, type:rep.data.media_type})
+			imgs.push({ url: rep.data.media_url, type: rep.data.media_type })
 		})
-	} 
+	}
 }
-
-	app.get('/frete',function(req,res){ 
-	res.setHeader("Access-Control-Allow-Origin", "*")	
-	console.log('Get parameter received are: ',req.query.frete)
-  	res.json([req.query.frete])
+	app.get('/frete', function (req, res) {
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	console.log('Get parameter received are: ', req.query.frete)
+	const options = {
+		method: 'GET',
+		url: 'https://sandbox.melhorenvio.com.br/api/v2/me/shipment/calculate',
+		headers: {
+			Accept: 'application/json',
+			Authorization: 'Bearer ' + process.env.FRETE,
+			'User-Agent': 'uauresinas (elexdragom@gmail.com)'
+		},
+		data: {
+			from: { postal_code: '65065689' },
+			to: { postal_code: req.query.frete },
+			products: [{ id: 'resina', width: 16, heitgh: 2, lenght: 24, weight: 0.2, quantity: 1 }]
+		}
+	};
+	axios.request(options).then(function (response) {
+		res.json(response.data[0].price)
 	})
+})
 	app.get('/instaURLS', function (req, res) {
-		console.log('i receive a GET request');
-		res.setHeader("Access-Control-Allow-Origin", "*")
-		res.json(imgs)
-	})
+	console.log('i receive a GET request');
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.json(imgs)
+})
 
-app.listen(port, () => {
+	app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 })
